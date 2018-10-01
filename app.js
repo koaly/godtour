2e107c4001b425d68b938e0821d1c36a2f2dd769
Key Person <pornmanuty@gmail.com>
2018-10-01 14:04:41 +0700

    init home page

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
-
 const app = express();
+const port = 3000
 
-app.listen(3000,function(){
-    console.log('Server start on port 3000')
+app.get('/',function(req,res){
+    res.send('Hello World');
+});
+
+app.listen(port,function(){
+    console.log(`Server start on port ${port}`)
 });
