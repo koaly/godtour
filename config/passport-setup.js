b41bd21f939b84ef3384709156316f3d9be68eac
Key Person <pornmanuty@gmail.com>
2018-10-02 19:54:31 +0700

    add serialzeUser

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -6,7 +6,19 @@ const GoogleStrategy = require('passport-google-oauth20');
  * I doesn't add this file to github you ask me if you want
  * it have a secret clientID and clientSecret that use google api
  */
-const keys = require('./keys.js')
+const keys = require('./keys.js');
+const User = require('../models/user-model');
+
+//serializeUser
+passport.serializeUser(function(user,done){
+    done(null,user.id);
+});
+
+
+passport.deserializeUser(function(id,done){
+    User.findById(id).then(user);
+    done(null,user.id);
+});
 
 passport.use(
     new GoogleStrategy({
@@ -14,8 +26,30 @@ passport.use(
         callbackURL:'/auth/google/redirect',
         clientID: keys.google.clientID,
         clientSecret: keys.google.clientSecret
+    
     },function(accessToken,refreshToken,profile,done){
-        console.log('passport callback');
-        console.log(profile);
+
+        //check user already exits in our db
+        User.findOne({googleId:profile.id})
+        .then(function(currentUser){
+            if(currentUser){
+                //already have the user
+                console.log('user is',currentUser);
+                done(null,currentUser);    
+            }
+            else{
+                //if not create user in our db
+                new User({
+                    username: profile.displayName,
+                    googleId: profile.id
+                })
+                .save()
+                .then(function(newUser){
+                    console.log('new user created'+newUser);
+                    done(null,newUser);
+                });
+            }
+        });
+        
     })
 );
--- models/user-model.js
+++ models/user-model.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    //user information
     username: String,
     googleId: String
 })
 
 const User = mongoose.model('user',userSchema);
 
+//export for passport-setup
 module.exports = User;
