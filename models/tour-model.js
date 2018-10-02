69cbb7d379cd5705430bd5c7204e57e5a191d522
koaly <natthapong.s@ku.th>
2018-10-02 21:43:51 +0700

    add tour attribute

--- models/tour-model.js
+++ models/tour-model.js
@@ -5,7 +5,76 @@ const articleSchema = mongoose.Schema({
         type: String,
         required: true
     },
-
+    organizer:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    destination:{
+        type: String,
+        required: true
+    },
+    day_duration:{
+        type: Number,
+        required: true
+    },
+    night_duration:{
+        type: Number,
+        required: true
+    },
+    start_book:{
+        type: Date,
+        required: true
+    },
+    end_book:{
+        type: Date,
+        required: true
+    },
+    start_trip:{
+        type: Date,
+        required: true
+    },
+    end_trip:{
+        type: Date,
+        required: true
+    },
+    flight_airline:{
+        type: String,
+        required: true
+    },
+    flight_airport:{
+        type: String,
+        required: true
+    },
+    flight_depart:{
+        type: String,
+        required: true
+    },
+    flight_return:{
+        type: String,
+        required: true
+    },
+    tour_grade:{
+        type: Number,
+        required: true
+    },
+    max_seat:{
+        type: Number,
+        required: true
+    },
+    now_seat:{
+        type: Number,
+        required: true
+    },
+    description:{
+        type: String
+    },
+    highlight:{
+        type: String
+    }
 });
 
 const Tour = mongoose.model('Tour', articleSchema);
