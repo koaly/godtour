cbbedf6c4dea1801d721eb72209edcd402c35033
koaly <natthapong.s@ku.th>
2018-10-02 22:24:15 +0700

    add tour route

--- routes/tour-routes.js
+++ routes/tour-routes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 // Bring in Tour Models
-let Tour = require('../models/tour');
+let Tour = require('../models/tour-model');
 
 // Add Route
 router.get('/add', ensureAuthenticated, function(req, res){
-    res.render('add_article', {
+    res.render('add_tour', {
         title: 'Add TOUR'
     });
 });
