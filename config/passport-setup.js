bd11d422806d274ce126b43246e9e1ea530cd403
Key Person <pornmanuty@gmail.com>
2018-10-02 19:23:25 +0700

    check keys

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -6,7 +6,7 @@ const GoogleStrategy = require('passport-google-oauth20');
  * I doesn't add this file to github you ask me if you want
  * it have a secret clientID and clientSecret that use google api
  */
-const keys = require('./none')
+const keys = require('./keys.js')
 
 passport.use(
     new GoogleStrategy({
