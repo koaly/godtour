640d16c5724ada6a7a686d196b4a9e216ead11ee
Key Person <pornmanuty@gmail.com>
2018-10-02 18:47:12 +0700

    remove key

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-//add this file to .gitignore
-module.exports = {
-    google:{
-        clientID:"998579355986-7h6rav0kinrmom2jcelejc8a33rl6nit.apps.googleusercontent.com",
-        clientSecret:"8S9xIk79NV_5jF4Xs8mjH9fv"
-    }
-}
