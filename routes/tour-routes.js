7a64fb2e67506fd14bdae754ecefae6c17d6c242
koaly <natthapong.s@ku.th>
2018-10-03 14:12:30 +0700

    eiei

--- routes/tour-routes.js
+++ routes/tour-routes.js
@@ -31,55 +31,13 @@ router.post('/add', function(req, res){
         tour.flight_airport = req.body.flight_airport;
         tour.flight_depart = req.body.flight_depart;
         tour.flight_return = req.body.flight_return;
-<<<<<<< HEAD
-=======
         tour.food = req.body.food;
->>>>>>> aa008ddb29c1a926b6e6e9f70724abc3839c62b8
         tour.stars = req.body.stars;
         tour.max_seat = req.body.max_seat;
         tour.now_seat = tour.max_seat;
         tour.description = req.body.description;
         tour.highlight = req.body.highlight;
         // wait for add date and time attributes
-<<<<<<< HEAD
-
-        tour.save(function(err){
-            if (err){
-                console.log(err);
-                return;
-            } else {
-                req.flash('success', 'New TOUR added');
-                res.redirect('/');
-            }
-        });
-    }
-});router.post('/add', function(req, res){
-
-    let err = req.validationErrors();
-    if (err){
-        res.render('add_tour', {
-            title: 'Add TOUR',
-            errors: err
-        });
-    } else{
-        let tour = new Tour();
-        tour.title = req.body.title;
-        tour.organizer = 'admin';
-        tour.price = req.body.price;
-        tour.destination = req.body.destination;
-        tour.day_duration = req.body.day_duration;
-        tour.night_duration = req.body.night_duration;
-        tour.flight_airline = req.body.flight_airline;
-        tour.flight_airport = req.body.flight_airport;
-        tour.flight_depart = req.body.flight_depart;
-        tour.flight_return = req.body.flight_return;
-        tour.stars = req.body.stars;
-        tour.max_seat = req.body.max_seat;
-        tour.now_seat = tour.max_seat;
-        tour.description = req.body.description;
-        tour.highlight = req.body.highlight;
-        // wait for add date and time attributes
-=======
         tour.start_book.setDate(req.body.start_book_date);
         tour.start_book.setMonth(req.body.start_book_month);
         tour.start_book.setFullYear(req.body.start_book_year);
@@ -96,7 +54,6 @@ router.post('/add', function(req, res){
         tour.end_trip.setMonth(req.body.end_trip_month);
         tour.end_trip.setFullYear(req.body.end_trip_year);
         tour.end_trip.setHour(req.body.return_hour, return_min, 0);
->>>>>>> aa008ddb29c1a926b6e6e9f70724abc3839c62b8
 
         tour.save(function(err){
             if (err){
