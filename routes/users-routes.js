e498d13906b53487a91d9a673e2e65acb0342343
koaly <natthapong.s@ku.th>
2018-10-06 18:31:38 +0700

    fix bug

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const User = require('../models/user-model');
 
 router.get('/',(req,res,next)=>{
