913a0656366aa588d0f4d35200549bb42ef1424c
Key Person <pornmanuty@gmail.com>
2018-10-13 10:02:36 +0700

    connect with routes

--- app.js
+++ app.js
@@ -3,7 +3,35 @@ const app = express();
 
 const morgan = require('morgan');
 
-app.use(morgan('dev'))
 
 
+app.use(morgan('dev'));
+
+
+
+const tourRoutes = require('./api/routes/tour');
+
+
+app.use("/tour",tourRoutes);
+
+
+
+//if not find path abover handling with 404
+app.use((req,res,next)=>{
+    const err = new Error("Not found");
+    err.status = 404;
+    next(err);
+})
+
+
+app.use((err,req,res,next)=>{
+    res.status(err.status || 500);
+    res.json({
+        error:{
+            message: err.message
+        }       
+    })
+})
+
+//export to server.js
 module.exports = app;
