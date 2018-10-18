e4e02f3bc6eb1cf369c6d19c8072f5d6b9771d8b
Key Person <pornmanuty@gmail.com>
2018-10-18 21:48:11 +0700

    init server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('api/customers', (req, res) => {
+    const customers = [
+        { id: 1, firstName: 'Jon', lastName: 'Doe' },
+        { id: 2, firstName: 'Porn', lastName: 'DoeAS' },
+        { id: 3, firstName: 'Piyawa', lastName: 'Dosse' },
+    ];
+    res.status(200).json(customers);
+})
+
+const port = 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
