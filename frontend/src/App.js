153a5be78a1e78c4f7ec4c4e938a230be4b7d907
nuttapon <nuttapon.wac@ku.th>
2018-10-19 00:31:05 +0700

    add route to /tour and /not-found

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import NavBar from "./components/navBar";
+import ShowTour from "./components/showTour";
+import HomePage from "./components/homePage";
+import NotFound from "./components/notFound";
+import { Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
@@ -7,6 +11,14 @@ class App extends Component {
     return (
       <React.Fragment>
         <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/tour" component={ShowTour} />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact to component={HomePage} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
       </React.Fragment>
     );
   }
