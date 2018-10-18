f6e758317ed594c83bc2a0a6f48490f98c47210e
Key Person <pornmanuty@gmail.com>
2018-10-18 21:37:25 +0700

    remove all

--- api/controllers/tour-controller.js
+++
@@ -1,6 +0,0 @@
-
-exports.getAll = (req,res,next) =>{
-    res.status(200).json({
-        hello : "world"
-    })
-}
--- api/routes/tour-router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const tourController = require('../controllers/tour-controller');
-
-router.get('/',tourController.getAll)
-
-
-module.exports = router
