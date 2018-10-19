15677b50832d83fb8e74dfbcce41185971f37fa1
koaly <natthapong.s@ku.th>
2018-10-20 00:02:50 +0700

    add tour await/async using

--- api/controllers/tour-controller.js
+++ api/controllers/tour-controller.js
@@ -24,37 +24,36 @@ exports.getAll = async function(req,res,next){
 }
 
 exports.addTour = async function(req, res, next){
-    const tour = await new Tour({
-        _id: new mongoose.Types.ObjectId,
-        name: req.body.name,
-        operatorID: "admin",
-        operatorName: "admin",
-        price: req.body.price,
-        dest: req.body.dest,
-        dayDuration: req.body.dayDuration,
-        nightDuration: req.body.nightDuration,
-        startBooking: req.body.startBookDate + 'T' + req.body.startBookTime,
-        endBooking: req.body.endBookDate + 'T' + req.body.endBookTime,
-        departDate: req.body.departDate,
-        returnDate: req.body.returnDate,
-        airline: req.body.airline,
-        maxSeat: req.body.seat,
-        currentSeat: req.body.seat,
-        food: req.body.food,
-        detail: req.body.detail,
-        highlight: req.body.highlight
-    })
-    tour.save()
-    .then(result =>{
+    try{
+        const tour = await new Tour({
+            _id: new mongoose.Types.ObjectId,
+            name: req.body.name,
+            operatorID: "admin",
+            operatorName: "admin",
+            price: req.body.price,
+            dest: req.body.dest,
+            dayDuration: req.body.dayDuration,
+            nightDuration: req.body.nightDuration,
+            startBooking: req.body.startBookDate + 'T' + req.body.startBookTime,
+            endBooking: req.body.endBookDate + 'T' + req.body.endBookTime,
+            departDate: req.body.departDate,
+            returnDate: req.body.returnDate,
+            airline: req.body.airline,
+            maxSeat: req.body.seat,
+            currentSeat: req.body.seat,
+            food: req.body.food,
+            detail: req.body.detail,
+            highlight: req.body.highlight
+        });
+        const result = await tour.save();
         console.log(result);
         res.status(201).json({
             message: "Tour added"
-        })
-    })
-    .catch(err =>{
+        });
+    } catch(err){
         console.log(err);
         res.status(500).json({
             error: err
         })
-    });
+    }
 }
