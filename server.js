354078aeb4eb08271fdd75fe2b3264e9b7fe2f5f
Key Person <pornmanuty@gmail.com>
2018-10-19 09:58:54 +0700

    spilt http and express

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
+const http = require('http');
+const app = require('./app')
 
-//just core need to add more next day
-const express = require('express');
+const port = process.env.PORT || 5000;
 
-const app = express();
+const server = http.createServer(app);
 
-app.get('/api/customers', (req, res) => {
-    const customers = [
-        { id: 1, firstName: 'Jon', lastName: 'Doe' },
-        { id: 2, firstName: 'Porn', lastName: 'DoeAS' },
-        { id: 3, firstName: 'Piyawa', lastName: 'Dosse' },
-    ];
-    res.status(200).json(customers);
-})
-
-const port = 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`))
+server.listen(port, () => console.log(`Server started on port ${port}`))
