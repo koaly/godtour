4c5f11fa2fac614e1c23a7b4abc498f226f57331
koaly <natthapong.s@ku.th>
2018-10-20 15:05:03 +0700

    can edit tour

--- api/controllers/tour-controller.js
+++ api/controllers/tour-controller.js
@@ -60,7 +60,31 @@ exports.addTour = async function(req, res, next){
 
 exports.editTour = async function(req, res, next){
     try{
+        const tour = {}
+        if(req.body.name) tour.name = req.body.name
+        if(req.body.price) tour.price = req.body.price
+        if(req.body.dest) tour.dest = req.body.dest
+        if(req.body.dayDuration) tour.dayDuration = req.body.dayDuration
+        if(req.body.nightDuration) tour.nightDuration = req.body.nightDuration
+        if(req.body.startBookDate && req.body.startBookTime) tour.startBooking = req.body.startBookDate + 'T' + req.body.startBookTime
+        if(req.body.endBookDate && req.body.endBookTime) tour.endBooking = req.body.endBookDate + 'T' + req.body.endBookTime
+        if(req.body.departDate) tour.departDate = req.body.departDate
+        if(req.body.returnDate) tour.returnDate = req.body.returnDate
+        if(req.body.airline) tour.airline = req.body.airline
+        if(req.body.maxSeat) tour.maxSeat = req.body.maxSeat
+        if(req.body.currentSeat) tour.currentSeat = req.body.currentSeat
+        if(req.body.food) tour.food = req.body.food
+        if(req.body.detail) tour.detail = req.body.detail
+        if(req.body.highlight) tour.highlight = req.body.highlight
         
+        console.log(req.params);
+        console.log(tour);
+        const id = {_id:req.params.id}
+        const result = await Tour.findOneAndUpdate(id, tour);
+        console.log(result);
+        res.status(202).json({
+            message: "Tour updated"
+        })
     } catch(err){
         console.log(err);
         res.status(500).json({
--- api/routes/tour-route.js
+++ api/routes/tour-route.js
@@ -5,6 +5,6 @@ const tourController = require('../controllers/tour-controller');
 
 router.get('/',tourController.getAll);
 router.post('/add',tourController.addTour);
-
+router.post('/edit/:id',tourController.editTour);
 
 module.exports = router
