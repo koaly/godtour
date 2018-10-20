bbca56ea6a22701202194e1fdad0abad55de98f4
nuttapon <nuttapon.wac@ku.th>
2018-10-20 18:38:27 +0700

    now homepage can see background object

--- client/src/components/homePage.jsx
+++ client/src/components/homePage.jsx
@@ -4,16 +4,19 @@ class HomePage extends Component {
   render() {
     return (
       <div>
-        <div className="sawaddee">
+        {/* <div className="sawaddee">
           <h3>.</h3>
           <h3>.</h3>
-        </div>
+        </div> */}
+
         <div className="homepage-image">
           <h1>Let's go to your world</h1>
-          <p>Find the best place to fulfill your wonderful vacay on TO.UR WORLD!</p>
+          <p>
+            Find the best place to fulfill your wonderful vacay on TO.UR WORLD!
+          </p>
         </div>
-        
-        
+
+        <h1>hi</h1>
       </div>
     );
   }
