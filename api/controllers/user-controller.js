741abd1722db95a792cae1ea534c10ddd890addd
Key Person <pornmanuty@gmail.com>
2018-10-20 10:00:57 +0700

    change to asyn/wait and promise

--- api/controllers/user-controller.js
+++ api/controllers/user-controller.js
@@ -3,80 +3,79 @@ const bcrypt = require('bcrypt');
 
 const User = require('../models/user-models');
 
-exports.getAll = (req,res,next) =>{
-    User.find()
-    .select()
-    .exec()
-    .then(docs =>{
-        console.log(docs);
-        const response ={
-            count: docs.length,
-            user: docs.map(doc =>{
-                return {
-                    email: doc.email,
-                    password: doc.password
-                }
-            })
-        }
-        res.status(200).json({
-            response
-        })
+const userResponse = (users) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const response = {
+                count: users.length,
+                user: users.map(doc => {
+                    return {
+                        email: doc.email,
+                        password: doc.password
+                    }
+                })
+            }
+            resolve(response);
+        }, 1000)
     })
-    .catch(err =>{
-        console.log(err);
+}
+exports.getAll = async (req, res, next) => {
+    try {
+        const users = await User.find()
+        const response = await userResponse(users)
+
+        res.status(200).json({
+            users: response
+        })
+    }
+    catch (err) {
         res.status(500).json({
             error: err
+        })
+    }
+}
+
+
+const createPassword = (password, salt) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.hash(password, salt, (err, encrypted) => {
+            console.log(err, " ", encrypted)
+            if (err) {
+                reject(err);
+            } else {
+                resolve(encrypted)
+            }
         })
     })
 }
 
-exports.userSignup = (req,res,next) =>{
-    User.find({
-        email: req.body.email
-    })
-    .exec()
-    .then(user =>{
-        console.log(user);
-        if(user.length >= 1){
+
+
+exports.userSignup = async (req, res, next) => {
+    try {
+        const user = await User.find({ email: req.body.email });
+        if (user.length >= 1) {
             return res.status(409).json({
                 message: "Email already existed"
             })
-        }else{
-            bcrypt.hash(req.body.password,10,(err,hash) =>{
-                if(err){
-                    return res.status(500).json({
-                        error: err,
-                        message: "Require Password"
-                    })
-                }else{
-                    const user = new User({
-                        _id: new mongoose.Types.ObjectId,
-                        email: req.body.email,
-                        password: hash
-                    })
-
-                    user
-                    .save()
-                    .then(result =>{
-                        console.log(result);
-                        res.status(201).json({
-                            message:"User Created"
-                        })
-                    })
-                    .catch(err =>{
-                        console.log(err);
-                        res.status(500).json({
-                            error: err
-                        })   
-                    })
-                }
-            })            
+        } else {
+            const hash = await createPassword(req.body.password, 10);
+            const newUser = await new User({
+                _id: new mongoose.Types.ObjectId,
+                email: req.body.email,
+                password: hash
+            })
+            const result = await newUser.save();
+            res.status(201).json({
+                message: "newUser Created",
+                user: result
+            })
         }
-    })
-    .catch(err =>{
-        console.log(err);
-        res.status(500).json({
+    }
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({
             error: err
         })
-    })
+    }
 }
