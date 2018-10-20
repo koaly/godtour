490d1250fefc0ef7b8b230f561208eeda8f0efab
Prempi <piyawat.pav@ku.th>
2018-10-20 15:54:32 +0700

    correct label on registerForm

--- client/src/components/registerForm.jsx
+++ client/src/components/registerForm.jsx
@@ -49,8 +49,8 @@ class RegisterForm extends Form {
         <h1>Register</h1>
         <form onSubmit={this.handleSubmit}>
           {this.renderInput("firstname", "Firstname")}
-          {this.renderInput("lastname", "lastname")}
-          {this.renderInput("username", "username")}
+          {this.renderInput("lastname", "Lastname")}
+          {this.renderInput("username", "Username")}
           {this.renderInput("email", "Email")}
           {this.renderInput("password", "Password", "password")}
           {this.renderSelect("gender", "Gender", this.state.gender)}
