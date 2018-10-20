f6c036e516ab888d8f8e1a8616a95ea6adc85c7c
nuttapon <nuttapon.wac@ku.th>
2018-10-21 00:26:10 +0700

    chicky profile

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -1,38 +1,21 @@
 import React, { Component } from "react";
+import { Link, NavLink } from "react-router-dom";
 import CancelBook from "./cancelBook";
 import MyBook from "./myBooking";
+import ProfileBar from "./common/profileBar";
 //import { Route, Switch, Redirect } from "react-router-dom";
 
 class Profile extends Component {
   render() {
     return (
-        <div>
-            <h1>Profile</h1>
-            <ul className="nav nav-pills flex-column">
-                <li className="nav-item active">
-                    <a className="nav-link" href="#">
-                        Profile
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">
-                        My Cards
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">
-                        Purchase List
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/profile/myBooking">
-                        My Booking
-                    </a>
-                </li>
-            </ul>
-            
+      <div className="container">
+        <h1>Profile</h1>
+        <div className="row">
+          <div className="col-md-4">
+            <ProfileBar />
+          </div>
         </div>
-        
+      </div>
     );
   }
 }
