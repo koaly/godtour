6bbb5332fc8015c0c7b680b1be243bc1832e89f7
Prempi <piyawat.pav@ku.th>
2018-10-20 23:44:09 +0700

    footer v1.0

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import "./footer.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+//import "bootstrap/dist/js/bootstrap.js";
 import "font-awesome/css/font-awesome.css";
+
 
 ReactDOM.render(
   <BrowserRouter>
