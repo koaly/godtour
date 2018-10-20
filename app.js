28ce4391676fe2c522c7dff36e566fea7093aed6
Key Person <pornmanuty@gmail.com>
2018-10-20 08:26:55 +0700

    remove warning

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ app.use(morgan('dev'));
 
 
 //connect to database
-mongoose.connect("mongodb://"+process.env.MONGO_MLAB_USER+":"
-                +process.env.MONGO_MLAB_PW+
-                "@ds121373.mlab.com:21373/tourworld",
-                (err) =>{
-                    if(err){
-                        return console.log(err);
-                    }
-                    console.log("Connected to MongoDB");
-                }
+mongoose.connect("mongodb://" + process.env.MONGO_MLAB_USER + ":"
+    + process.env.MONGO_MLAB_PW +
+    "@ds121373.mlab.com:21373/tourworld", { useNewUrlParser: true },
+    (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Connected to MongoDB");
+    }
 )
 
 mongoose.Promise = global.Promise;
@@ -33,12 +33,12 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
     }
     next();
 });
@@ -50,25 +50,25 @@ const userRoutes = require('./api/routes/user-route');
 
 
 // express use routes to create path
-app.use("/tours",tourRoutes);
-app.use("/users",userRoutes);
+app.use("/tours", tourRoutes);
+app.use("/users", userRoutes);
 
 
 
 //if not find path abover handling with 404
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     const err = new Error("Not found");
     err.status = 404;
     next(err);
 })
 
 
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.json({
-        error:{
+        error: {
             message: err.message
-        }       
+        }
     })
 })
 
