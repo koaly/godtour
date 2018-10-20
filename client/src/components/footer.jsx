4acdf92d2217e4eb5147cc057ea51e4dfd6182db
Prempi <piyawat.pav@ku.th>
2018-10-20 18:18:28 +0700

    mdi

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Link, NavLink } from "react-router-dom";
+import {TwitterIcon} from 'mdi-react'
 
 const Footer = () => {
     return (
         <div className="footer">
+            
             <p>TO-UR WORLD, Copyright © 2018</p>
+
         </div>
     );
 };
