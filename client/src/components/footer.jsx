fda5d2289a8891d4f2d8b2e287ae58e930a78168
nuttapon <nuttapon.wac@ku.th>
2018-10-20 18:37:34 +0700

    update footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div className="footer">
-            <p>TO-UR WORLD, Copyright © 2018</p>
-        </div>
-    );
+  return (
+    <div className="footer">
+      <p>TO-UR WORLD, Copyright © 2018</p>
+    </div>
+  );
 };
 export default Footer;
