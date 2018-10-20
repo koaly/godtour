61f1dceda1de7efe27071c164e8ec4b6914797a7
Prempi <piyawat.pav@ku.th>
2018-10-20 16:36:44 +0700

    solve footer chicky2

--- client/src/components/homePage.jsx
+++ client/src/components/homePage.jsx
@@ -4,14 +4,15 @@ class HomePage extends Component {
   render() {
     return (
       <div>
-        <div className="homepage-image">
-          <h1>Let's go to your world</h1>
-          <p>Find the best place to fulfill your wonderful vacay on TO.UR WORLD!</p>
-        </div>
         <div className="sawaddee">
           <h3>.</h3>
           <h3>.</h3>
         </div>
+        <div className="homepage-image">
+          <h1>Let's go to your world</h1>
+          <p>Find the best place to fulfill your wonderful vacay on TO.UR WORLD!</p>
+        </div>
+        
         
       </div>
     );
