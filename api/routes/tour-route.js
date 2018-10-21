4fba27245395a0c38b93593d90d652e070468350
koaly <natthapong.s@ku.th>
2018-10-21 17:13:51 +0700

    add auth on tour-route

--- api/routes/tour-route.js
+++ api/routes/tour-route.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const tourController = require('../controllers/tour-controller');
 
-router.get('/',tourController.getAll);
-router.post('/add',tourController.addTour);
-router.put('/edit/:id',tourController.editTour);
-router.delete('/:id',tourController.deleteTour);
+router.get('/', auth.optional, tourController.getAll);
+router.post('/add', auth.require, tourController.addTour);
+router.put('/edit/:id', auth.require, tourController.editTour);
+router.delete('/:id', auth.require, tourController.deleteTour);
 
 module.exports = router
