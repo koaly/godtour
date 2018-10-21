0f285d8eed138660caca69d943aa6f6ccfaafc16
koaly <natthapong.s@ku.th>
2018-10-22 03:32:04 +0700

    require Booking

--- api/controllers/tour-controller.js
+++ api/controllers/tour-controller.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const Tour = require('../models/tour-models');
 const User = require('../models/user-models');
+const Booking = require('../models/booking-models');
 
 exports.getAll = async function(req,res,next){
     try{
