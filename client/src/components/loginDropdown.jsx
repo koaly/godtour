82e649098645f90c4f86428414d59f91e4fc7b4e
nuttapon <nuttapon.wac@ku.th>
2018-10-21 15:33:12 +0700

    separate login drop down to loginDropdown component

---
+++ client/src/components/loginDropdown.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from "react";
+import { Link, NavLink } from "react-router-dom";
+import Form from "./common/form";
+import Joi from "joi-browser";
+
+class LoginDropdown extends (Component, Form) {
+  state = {
+    data: { email: "", password: "" },
+    errors: {}
+  };
+
+  schema = {
+    email: Joi.string()
+      .required()
+      .email()
+      .label("Email"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+
+  doSubmit = () => {
+    console.log("Submitted");
+  };
+  render() {
+    return (
+      <li className="dropdown nav-item space">
+        <NavLink
+          className="nav-link dropdown-toggle"
+          to="/login"
+          data-toggle="dropdown"
+        >
+          Login
+        </NavLink>
+        <ul id="login-dp" className=" dropdown-menu dropdown-menu-right">
+          <li>
+            <div className="row">
+              <div className="col-md-12">
+                Login With
+                <div className="social-buttons">
+                  <a href="#">
+                    <button className="btn btn-block btn-google">
+                      <i className="fa fa-google">Google</i>
+                    </button>
+                  </a>
+                </div>
+                or
+                <form onSubmit={this.handleSubmit}>
+                  {this.renderInput("email", "Email")}
+                  {this.renderInput("password", "Password", "password")}
+                  {this.renderButton("Login")}
+                </form>
+              </div>
+            </div>
+            <div className="bottom text-center">
+              New here ?<Link to="/register">Register</Link>
+            </div>
+          </li>
+        </ul>
+      </li>
+    );
+  }
+}
+
+export default LoginDropdown;
