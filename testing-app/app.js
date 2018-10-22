f8b53d3a553b99309e09ae324911b71c5f859cf3
Key Person <pornmanuty@gmail.com>
2018-10-22 09:23:06 +0700

    fix

--- testing-app/app.js
+++ testing-app/app.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+
+const morgan = require('morgan');
+//use morgan to tracking request
+app.use(morgan('dev'));
+
+
+app.use('/', require('./routes'));
+
+app.listen(5000);
