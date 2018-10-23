068da4877c64fc994fcd156308eb894ad8141afd
Supasan Komonlit <supasan_224@hotmail.com>
2018-10-23 19:51:51 +0700

    Have concept to get value

---
+++ fetch_module/app_test.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app_test = express()
+const port = 4000
+var fetch_from_url = require("fetch").fetchUrl;
+var fetch_from_stream = require("fetch").FetchStream;
+var count = 0;
+var test_01;
+var test_02;
+
+app_test.use('/' , (req , res) => res.send('Hello World'))
+
+var fetch = new fetch_from_stream("http://localhost:5000/users");
+
+fetch.on("meta" , function(responseHeaders){
+	console.log("<--------- fetch on meta --------->")
+	console.log( responseHeaders )
+});
+
+fetch.on("data" , function(chunk){
+	console.log("<--------- fetch on data --------->")
+	console.log( chunk )
+	if( count == 0){
+		test_01 = chunk
+		count += 1
+	}
+	else{
+		test_02 = chunk
+	}
+	console.log("<--------- finish chunk --------->")
+});
+
+//var fetch_end = new fetch_from_stream("http://localhost:5000/users" , { cookieJar : temporary});
+
+fetch.on("end" , function(){
+	console.log("<-------- fetch_end ------------>")
+	console.log("test_01")
+	console.log(test_01)
+	console.log("test_02")
+	console.log(test_02)
+	console.log("sum_test")
+	console.log( test_01 + test_02)
+});
+
+app_test.listen( port , () => console.log('app_test listen on port ' + port))
+
+module.exports = app_test
