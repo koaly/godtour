a6e7c79ddacd2bcbed767cd659c7baea3f2786d4
Prempi <piyawat.pav@ku.th>
2018-10-23 11:19:47 +0700

    new form style, add form.css

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./footer.css";
+import "./form.css";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
