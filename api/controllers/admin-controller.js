763fae8125b10fdae76621525032aa2e5069c59d
koaly <natthapong.s@ku.th>
2018-10-23 23:18:23 +0700

    can responà¸d a request

--- api/controllers/admin-controller.js
+++ api/controllers/admin-controller.js
@@ -4,6 +4,49 @@ const Tour = require('../models/tour-models');
 const User = require('../models/user-models');
 const Booking = require('../models/booking-models');
 
+exports.checkAdminStatus = async (req, res, next) => {
+    try{
+        const { payload: { id } } = req;
+        const user = await User.findById(id);
+        console.log(user.status);
+        if(user.status != 2){
+            return res.status(403).json({
+                error: {
+                    message: "Permission denied"
+                }
+            });
+        } else{
+            return next();
+        }
+    } catch(err){
+        console.log(err)
+        return res.status(500).json({
+            error: err
+        });
+    } 
+}
+
+exports.checkUpgradeRequest = async (req, res, next) => {
+    try{
+        const user = await User.findById(req.params.id);
+        console.log(user.upgradeRequest);
+        if(!user.upgradeRequest){
+            return res.status(403).json({
+                error: {
+                    message: "No Request"
+                }
+            });
+        } else{
+            return next();
+        }
+    } catch(err){
+        console.log(err)
+        return res.status(500).json({
+            error: err
+        });
+    } 
+}
+
 exports.getUpgradeRequest = async (req, res, next) => {
     try{
         const users = await User.find({upgradeRequest: true})
@@ -13,6 +56,43 @@ exports.getUpgradeRequest = async (req, res, next) => {
         res.status(200).json({
             count : users.length,
             users
+        });
+    } catch(err){
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    }
+}
+
+exports.acceptUpgradeRequest = async (req, res, next) => {
+    try{
+        const user = await User.findById(req.params.id);
+        console.log(user);
+        user.upgradeRequest = false;
+        user.status = 1;
+        const result = await user.save();
+        console.log(result);
+        res.status(200).json({
+            message: "accepted request"
+        });
+    } catch(err){
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    }
+}
+
+exports.refuseUpgradeRequest = async (req, res, next) => {
+    try{
+        const user = await User.findById(req.params.id);
+        console.log(user);
+        user.upgradeRequest = false;
+        const result = await user.save();
+        console.log(result);
+        res.status(200).json({
+            message: "refused request"
         });
     } catch(err){
         console.log(err);
--- api/routes/admin-route.js
+++ api/routes/admin-route.js
@@ -7,6 +7,8 @@ const operatorCtrl = require('../controllers/operator-controller');
 const bookingCtrl = require('../controllers/booking-controller');
 const auth = require('./auth');
 
-router.get('/request/upgrade', auth.require, adminCtrl.getUpgradeRequest);
+router.get('/request/upgrade', auth.require, adminCtrl.checkAdminStatus, adminCtrl.getUpgradeRequest);
+router.put('/request/upgrade/:id/accept', auth.require, adminCtrl.checkAdminStatus, adminCtrl.checkUpgradeRequest, adminCtrl.acceptUpgradeRequest);
+router.put('/request/upgrade/:id/refuse', auth.require, adminCtrl.checkAdminStatus, adminCtrl.checkUpgradeRequest, adminCtrl.refuseUpgradeRequest);
 
 module.exports = router;
