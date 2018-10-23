549d350bc83e7644d85d7782c06bdb2df5c10e7f
Key Person <pornmanuty@gmail.com>
2018-10-23 13:21:55 +0700

    i can't fetch some one help me

--- client/src/components/homepage/homePage.jsx
+++ client/src/components/homepage/homePage.jsx
@@ -1,82 +1,61 @@
 import React, { Component } from "react";
-import axios from 'axios';
 import './hompage.css'
 
-
-//GET URL
-const API = "http://localhost:5000/";
+const URL = 'https://jsonplaceholder.typicode.com/users'
 class HomePage extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      data: [],
-      isLoading: false,
-      error: null,
+      users: [],
+      isLoaded: false,
     }
   }
-  //when load this componet do
-  async componentDidMount() {
-    try {
-      this.setState({ isLoading: true })
-      //axios like fetch
-      const response = await axios.get(API);
-      this.setState(
-        {
-          data: response.data,
-          isLoading: false,
-        }
-      );
-    } catch (error) {
-      this.setState({
-        error,
-        isLoading: false,
+  //do when this componnet is load
+  componentDidMount() {
+    this.getUser();
+  }
+  //fetch user from localhost:5000/
+  getUser() {
+    //don't need to config proxy
+    fetch(URL)
+      .then(res => {
+        res.json()
       })
-    }
+      .then(json => {
+        return this.setState({
+          users: json,
+          isLoaded: true
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
-  getMessage() {
-    return this.state.data.message;
+  renderUser() {
+    return this.state.users.map(user => (
+      <li>{user.message}</li>
+    ))
   }
   render() {
-    const { data, isLoading, error } = this.state;
-    //if not fetch
-    if (error) {
-      return <div><h1>Run cd test-app && npm start first{error.message}</h1></div>
-    }
-    //if fetching
-    if (isLoading) {
-      return <div><h1>Loading </h1></div>
-    }
-    console.log(JSON.stringify(data));
-    //some one plz print json 
-    //i can't
-    /*
-    if (data) {
-      const nameList = data.users.map(function (user) {
-        console.log(JSON.stringify(user));
-        return <p>hi</p>
-      })
-    }
-    */
-    return (
+    const { isLoaded, users } = this.state;
+    if (!isLoaded) {
+      return <h1>Loading</h1>
+    } else {
 
-      <div>
-        <div className="homepage-image">
-          <h1>Let's go to your world</h1>
-          <p>
-            Find the best place to fulfill your wonderful vacay on TO.UR WORLD!
-          </p>
+      return (
+        <div className="HomePage">
+          <ul>
+            {users && users.map(user => (
+              <li key={user.id}>
+                {user.name}|{user.email}
+              </li>
+            ))};
+        </ul>
         </div>
-        <div>
-        </div>
-        <h1>
-          {this.getMessage()}
-        </h1>
-        <div>
-        </div>
-      </div>
-    );
+      )
+    }
   }
 }
 
--- testing-app/routes.js
+++ testing-app/routes.js
@@ -3,12 +3,8 @@ const router = express.Router();
 
 
 router.get('/', (req, res, next) => {
-    return res.status(200).json({
-        message: "hello from backend",
-        users: [
-            { _id: 1, Email: "suck@gmail.com" },
-            { _id: 2, Email: "suck2@gmail.com" },
-        ]
-    });
+    res.status(200).json({
+        "message": "hello world"
+    })
 })
 module.exports = router
