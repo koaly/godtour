b0b27bdde56672f93dd2e7f9d25a5326a5015201
Prempi <piyawat.pav@ku.th>
2018-10-23 20:58:07 +0700

    corousel ja

--- client/src/components/homepage/homePage.jsx
+++ client/src/components/homepage/homePage.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import './hompage.css'
+import './hompage.css';
+
 
 const URL = 'https://jsonplaceholder.typicode.com/users'
 class HomePage extends Component {
@@ -46,19 +47,49 @@ class HomePage extends Component {
 
       return (
         <div className="HomePage">
-          <div className="homepage-image">
-          <h1>Let's go to your world</h1>
-          <p>
-            Find the best place to fulfill your wonderful vacay on TO.UR WORLD!
-          </p>
-        </div>
+  
+          {/*<div classNameName="homepage-image">
+            <h1>Let's go to your world</h1>
+            <p>
+              Find the best place to fulfill your wonderful vacay on TO.UR WORLD!
+            </p>
+            </div>
+          
+             */}
+            <div id="carouselExampleIndicators" className="carousel slide mb-5" data-ride="carousel">
+              <ol className="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              </ol>
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img className="d-block w-100" src="https://www.japan-guide.com/thumb/XYZeXYZe2172_1680.jpg" alt="First slide"/>
+                </div>
+                <div className="carousel-item">
+                  <img className="d-block w-100" src="https://www.japan-guide.com/thumb/XYZeXYZe2172_1680.jpg" alt="Second slide"/>
+                </div>
+                <div className="carousel-item">
+                  <img className="d-block w-100" src="https://www.japan-guide.com/thumb/XYZeXYZe2172_1680.jpg" alt="Third slide"/>
+                </div>
+              </div>
+              <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="sr-only">Previous</span>
+              </a>
+              <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+              </a>
+            </div>
+        
           <ul>
             {users && users.map(user => (
               <li key={user.id}>
                 {user.name}|{user.email}
               </li>
             ))};
-        </ul>
+          </ul>
         </div>
       )
     }
