053a0e53573aab00b79e05f1251e2394842817c6
nuttapon <nuttapon.wac@ku.th>
2018-10-24 14:19:13 +0700

    cancelbook is now responsive. maybehaha

--- client/src/components/cancelBook.jsx
+++ client/src/components/cancelBook.jsx
@@ -5,19 +5,22 @@ import { TrashCanIcon, CancelIcon } from "mdi-react";
 class CancelBook extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="cancel-booking">
-          <center className="mt-4 mb-4">
-            <h1 className="mb-4 ">Are you sure ?</h1>
-            <button className="btn btn-danger btn-lg mb-4 mr-4 ">
-              Delete  <TrashCanIcon className="ml-1"/>
-
-            </button> 
-            <button className="btn btn-primary btn-lg mb-4">
-              Cancel  <CancelIcon className="ml-1"/>
-            </button> 
-          </center> 
+      <div class="row">
+        <div class="col-md-3">hahaไม่เลว</div>
+        <div class="col-md-6">
+          <div className="cancel-booking">
+            <center className="mt-2 mb-2">
+              <h1 className="mb-4 ">Are you sure ?</h1>
+              <button className="btn btn-danger btn-lg mb-4 mr-4 ">
+                Delete <TrashCanIcon className="ml-1" />
+              </button>
+              <button className="btn btn-primary btn-lg mb-4">
+                Cancel <CancelIcon className="ml-1" />
+              </button>
+            </center>
+          </div>
         </div>
+        <div class="col-md-3">hahaไม่เลว</div>
       </div>
     );
   }
