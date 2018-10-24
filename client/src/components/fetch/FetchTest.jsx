df2dbda5f4ea1a4101508e7f9b34f65fece42b31
Supasan Komonlit <supasan_224@hotmail.com>
2018-10-25 05:03:45 +0700

    I'm surrender to use heritage

--- client/src/components/fetch/FetchTest.jsx
+++ client/src/components/fetch/FetchTest.jsx
@@ -9,7 +9,6 @@ class FetchClass extends React.Component{
 	constructor( props ){
 		console.log("In constructor");
 		super( props );
-		this.fetch_class = new FetchAllUsers( this.callback ,this )
 		this.finish = false;
 		this.state = {
 			error: null,
@@ -17,37 +16,31 @@ class FetchClass extends React.Component{
 			num_res: 0,
 			url: ""
 		}
-		// if you run this you will see render have run before get_data finished
-	/*		
-		[ this.header , this.data ] = this.fetch_class.get_data()
-		console.log("Finish get data ")
-		this.finish = true
-		this.forceUpdate()
-	*/
-		// didn't good because not sure I think rendor run after this constructor finish	
 	}
 
 	componentDidMount(){ // this function call when rendor first time
-		this.fetch_class.get_all_user()
+		this.fetch_class = new FetchAllUsers( this.callback ,this )
+		//this.fetch_class.get_all_user()
+		this.fetch_class.get_specific_email( "suck2@gmail.com")
 		console.log("Finish get data ")
 	}
 
 	callback( information , data , this_){
-		console.log("<---------- In callback function ------------>")
-		console.log("--------> receive data")
-		console.log(information)
-		console.log(data)
-		console.log("--------> Information ")
-		this_.state.num_res = information.status
+		console.log("<---------- In callback function ------------>");
+		console.log("--------> receive data");
+		console.log(information);
+		console.log(data);
+		console.log("--------> Information ");
+		this_.state.num_res = information.status;
 		this_.state.url = information.url;
 		console.log( this_.state );
 		this_.data = data;	
-		console.log("--------> data ")
+		console.log("--------> data ");
 		console.log( this_.data );
-		console.log("--------> change isLoading")
-		this_.state.isLoading = true
-		this_.finish = true
-		console.log("--------> finsh change isLoading")
+		console.log("--------> change isLoading");
+		this_.state.isLoading = true;
+		this_.finish = true;
+		console.log("--------> finsh change isLoading");
 	}
 
 	render(){
@@ -56,15 +49,8 @@ class FetchClass extends React.Component{
 		console.log("error is " + error);
 		console.log("isLoading is " + isLoading );
 // for use react table
-		const columns = [
-							{ Header : "number id" , accessor: "_id" },
-							{ Header : "users email" , accessor: "email"},
-							{ Header : "use Google ID?" , accessor: "isGoogle"},
-							{ Header : "name Google ID" , accessor: "googleID"},
-							{ Header : "register date" , accessor: "registerDate"}
-						]
 		if( this.finish ){
-			const data = this.data.users.user
+			const data = this.data
 			return(
 				<div>
 					<h1>Welcome to page for testing fetch</h1>
@@ -74,7 +60,15 @@ class FetchClass extends React.Component{
 						<li>&ensp;last url is {this.state.url}</li>
 					</ul>
 					<h2>Data</h2>
-					<ReactTable data = {data} columns={columns}/>
+					<ReactTable data = {data} 
+						columns={[
+							{ Header: "user_id" , accessor : "_id"}
+							,{ Header: "email" , accessor : "email"}
+							,{ Header: "is_google?" , accessor : "isGoogle"}
+							,{ Header: "google_id" , accessor : "googleID"}
+							,{ Header: "register_data" ,accessor : "registerDate"} 
+						]}
+					/>
 				</div>
 			);
 		}
