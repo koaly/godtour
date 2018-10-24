725479cf63c5113854cc8cc559b82af444a7934e
Supasan Komonlit <supasan_224@hotmail.com>
2018-10-24 23:40:59 +0700

    Knowing step of class

--- client/src/components/fetch/FetchTest.jsx
+++ client/src/components/fetch/FetchTest.jsx
@@ -7,6 +7,7 @@ class FetchClass extends React.Component{
 	constructor( props ){
 		console.log("In constructor");
 		super( props );
+		this.fetch_class = new FetchMain("users")
 		this.finish = false;
 		this.list_item = [];
 		this.state = {
@@ -15,36 +16,11 @@ class FetchClass extends React.Component{
 			num_res: 0,
 			url: ""
 		}
+		// if you run this you will see render have run before get_data finished
+		[ this.header , this.data ] = this.fetch_class.get_data()
 	}
 
-	componentDidMount(){
-		console.log("In function componentDidMount");
-		this.fetch_data();
-	}
 
-	fetch_data(){
-		console.log("In function fetch_data");
-		fetch("http://localhost:5000/users")
-			.then( response => {
-				console.log( "status of response is " + response.status)
-				this.state.num_res = response.status 
-				this.state.url = response.url
-				return response.json()
-			})
-			.then( json => { 
-				console.log( json );
-				console.log( "type of json " + typeof( json ) );
-				this.finish = true;
-				this.list_item = json;
-				console.log( "finish setstate")
-				console.log( typeof(this.state.isLoading) )
-				this.state.isLoading = this.finish
-				console.log( this.state )
-				this.forceUpdate()
-				console.log("After forceUpdate")
-			})
-	}
-	
 	render(){
 		console.log("In render")
 		const { error ,isLoading , item} = this.state;
