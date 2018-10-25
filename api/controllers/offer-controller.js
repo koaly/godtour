1a23f451df605b1b6216300cacfcb23d57b260ad
koaly <natthapong.s@ku.th>
2018-10-26 00:22:02 +0700

    addOffer

--- api/controllers/offer-controller.js
+++ api/controllers/offer-controller.js
@@ -20,3 +20,37 @@ exports.getByTiy = async (req, res, next) => {
         });
     }
 }
+
+exports.addOffer = async function(req, res, next){
+    try{
+        const { payload: { id, email } } = req;
+        const offer = await new Offer({
+            _id: new mongoose.Types.ObjectId,
+            name: req.body.name,
+            tiyID: req.params.tiyID,
+            operatorID: id,
+            operatorName: email,
+            price: req.body.price,
+            dest: req.body.dest,
+            dayDuration: req.body.dayDuration,
+            nightDuration: req.body.nightDuration,
+            departDate: req.body.departDate,
+            returnDate: req.body.returnDate,
+            airline: req.body.airline,
+            member: req.body.member,
+            food: req.body.food,
+            detail: req.body.detail,
+            highlight: req.body.highlight
+        });
+        const result = await offer.save();
+        console.log(result);
+        res.status(201).json({
+            message: "Offer added"
+        });
+    } catch(err){
+        console.log(err);
+        res.status(500).json({
+            error: err
+        })
+    }
+}
