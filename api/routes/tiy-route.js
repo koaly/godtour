a4d112a67b6c782e9a3e303083c2083943527b5a
koaly <natthapong.s@ku.th>
2018-10-27 03:55:40 +0700

    many routes

--- api/routes/tiy-route.js
+++ api/routes/tiy-route.js
@@ -23,13 +23,23 @@ router.get('/create', auth.require, operatorCtrl.checkNonOperatorStatus, async (
 });
 router.post('/create', auth.require, operatorCtrl.checkNonOperatorStatus, tiyCtrl.addTiy);
 router.get('/:tiyID', auth.require, tiyCtrl.checkOwnTiyPlus, tiyCtrl.checkNonAccepted, tiyCtrl.getOneTiy);
+router.post('/tiyID', auth.require, tiyCtrl.checkOwnTiy, tiyCtrl.cancelOffer); 
 router.delete('/:tiyID', auth.require, tiyCtrl.checkOwnTiy, tiyCtrl.deleteTiy);
 router.get('/:tiyID/edit', auth.require, tiyCtrl.checkOwnTiy, tiyCtrl.getOneTiy);
 router.put('/:tiyID/edit', auth.require, tiyCtrl.checkOwnTiy, tiyCtrl.editTiy);
 
 router.get('/:tiyID/offers', auth.require, tiyCtrl.checkOwnTiy, offerCtrl.getByTiy);
+router.get('/:tiyID/offers/create', auth.require, operatorCtrl.checkOperatorStatus, async (req, res) => {
+    res.status(200).json({
+        'message': "add offer page"
+    });
+});
+router.post('/:tiyID/offers/create', auth.require,operatorCtrl.checkOperatorStatus, offerCtrl.addOffer);
 router.get('/:tiyID/offers/:offerID', auth.require, offerCtrl.checkOwnOfferPlus, offerCtrl.getOneOffer);
 router.post('/:tiyID/offers/:offerID', auth.require, tiyCtrl.checkOwnTiy, tiyCtrl.acceptOffer);
 router.delete('/:tiyID/offers/:offerID', auth.require, offerCtrl.checkOwnOffer, offerCtrl.deleteOffer);
+
+router.get('/:tiyID/offers/:offerID/edit', auth.require, offerCtrl.checkOwnOffer, offerCtrl.getOneOffer);
+router.post('/:tiyID/offers/:offerID/edit', auth.require, offerCtrl.checkOwnOffer, offerCtrl.editOffer);
 
 module.exports = router
