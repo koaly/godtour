c0ffe5f65817d7d00d3ad2c8c57955da18f3d615
Supasan Komonlit <supasan_224@hotmail.com>
2018-10-27 23:04:47 +0700

    Init tour

--- client/src/components/showTour.jsx
+++ client/src/components/showTour.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
+import styles from "./../css/showtour.css"
 
 class ShowTour extends Component {
-  render() {
-    return (
-      <div className="container mgtb">
-        <h1>Tour List</h1>
-      </div>
-    );
-  }
+	
+	constructor( props ){
+		super( props )
+		this.state = {	Loading		: true
+
+		}
+		this.ShowMoreCallback = this.ShowMoreCallback.bind(this)
+	}
+
+	ShowMoreCallback(){
+		console.log("===============>ShowTour:ShowMoreCallback")
+	}
+
+	render() {
+		return (
+			<div className="container mgtb">
+				<h1>Tour List</h1>
+				<button	className	= "ButtonMoreTour"
+						onClick		= { this.ShowMoreCallback }
+				>
+					{ this.state.Loading ? 'Loading' : 'More Tour!' }
+				</button>
+			</div>
+		);
+	}
 }
 
 export default ShowTour;
