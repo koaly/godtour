f457fd5a2e41cd9b8a64f6b0c352b5ff3bde0f9d
Supasan Komonlit <supasan_224@hotmail.com>
2018-10-29 00:52:44 +0700

    Finish callback function

--- client/src/components/showTour.jsx
+++ client/src/components/showTour.jsx
@@ -1,29 +1,47 @@
 import React, { Component } from "react";
-import styles from "./../css/showtour.css"
+import styles from "./../css/showtour.css";
+import FetchAllTours from "./fetch/FetchAllTours";
 
 class ShowTour extends Component {
 	
 	constructor( props ){
 		super( props )
-		this.state = {	Loading		: true
-
+		this.state = {	
+			Loading		: true
 		}
 		this.ShowMoreCallback = this.ShowMoreCallback.bind(this)
+		this.FetchReceiveTourCallback = this.FetchReceiveTourCallback.bind(this)
+		this.FetchAllTours = new FetchAllTours();
 	}
 
 	ShowMoreCallback(){
-		console.log("===============>ShowTour:ShowMoreCallback")
+		console.log("===============> ShowTour:ShowMoreCallback");
+		this.setstate( state => ({
+			Loading		: true
+		}));	
+	}
+
+	componentDidMount(){
+		console.log("===============> ShowTour:componentDidMount");
+		this.FetchAllTours.get_all_tours( 0 , 5 , this.FetchReceiveTourCallback );
+	}
+
+	FetchReceiveTourCallback( ReceiveInformation , ReceiveData ){
+		console.log("===============> ShowTour.FetchReceiveTourCallback" , ReceiveData );
+		this.setState( state => ({ 
+			Loading		: false 
+		}));
 	}
 
 	render() {
+		console.log("===============> Show_tour.render()")
 		return (
 			<div className="container mgtb">
 				<h1>Tour List</h1>
-				<button	className	= "ButtonMoreTour"
-						onClick		= { this.ShowMoreCallback }
-				>
-					{ this.state.Loading ? 'Loading' : 'More Tour!' }
-				</button>
+				{ this.state.Loading &&	<button	className = "GeneralButtonTour" > 'Now Loading!' 
+					</button> }
+				{ ! this.state.Loading && <button className = "ButtonMoreTour GeneralButtonTour"
+						onClick = { this.ShowMoreCallback }> "More Tour!" </button>}
 			</div>
 		);
 	}
