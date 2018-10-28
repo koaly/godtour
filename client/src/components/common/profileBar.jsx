5b2e81f3de5171c51e1b669cec0f2e3b9bd9e08d
Prempi <piyawat.pav@ku.th>
2018-10-28 19:15:04 +0700

    pdl

--- client/src/components/common/profileBar.jsx
+++ client/src/components/common/profileBar.jsx
@@ -7,30 +7,30 @@ class ProfileBar extends Component {
     console.log(userr);
     return (
       <div>
-        <ul className="nav flex-column nav-pills pdb">
+        <ul className="nav flex-column nav-pills pdb pdl">
           {userr && (
             <img
-              className="profileimg"
+              className="profileimg pdl"
               src={userr.info.imgsrc}
               alt="no image"
             />
           )}
-          <li className="nav-item">
+          <li className="nav-item pdl">
             <NavLink className="nav-link" exact to="/profile">
               <span className="black">Profile</span>
             </NavLink>
           </li>
-          <li className="nav-item">
+          <li className="nav-item pdl">
             <NavLink className="nav-link" to="/profile/myCard">
             <span className="black">My Cards</span>
             </NavLink>
           </li>
-          <li className="nav-item">
+          <li className="nav-item pdl">
             <NavLink className="nav-link" to="/profile/purchaseList">
             <span className="black">Purchase List</span>
             </NavLink>
           </li>
-          <li className="nav-item">
+          <li className="nav-item pdl">
             <NavLink className="nav-link" to="/profile/myBooking">
             <span className="black">My Booking</span>
             </NavLink>
