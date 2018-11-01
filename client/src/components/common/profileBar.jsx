17630149157dab963ba2fced7a67f8d5ae7e4c9c
Key Person <pornmanuty@gmail.com>
2018-11-01 17:44:58 +0700

    fix profile suck you jui

--- client/src/components/common/profileBar.jsx
+++ client/src/components/common/profileBar.jsx
@@ -3,36 +3,33 @@ import { Link, NavLink } from "react-router-dom";
 
 class ProfileBar extends Component {
   render() {
-    const { userr } = this.props;
-    console.log(userr);
+    const { info } = this.props;
     return (
       <div>
         <ul className="nav flex-column nav-pills pdb pdl">
-          {userr && (
-            <img
-              className="profileimg pdl mgt"
-              src={userr.info.imgsrc}
-              alt="no image"
-            />
-          )}
+          <img
+            className="profileimg pdl mgt"
+            src={info && info.imgsrc}
+            alt="no image"
+          />
           <li className="nav-item pdl">
-            <NavLink className="nav-link" exact to="/profile">
+            <NavLink className="nav-link" exact to="/profile" info={info}>
               <span className="black">Profile</span>
             </NavLink>
           </li>
           <li className="nav-item pdl">
-            <NavLink className="nav-link" to="/profile/myCard">
-            <span className="black">My Cards</span>
+            <NavLink className="nav-link" to="/profile/myCard" info={info}>
+              <span className="black">My Cards</span>
             </NavLink>
           </li>
           <li className="nav-item pdl">
             <NavLink className="nav-link" to="/profile/purchaseList">
-            <span className="black">Purchase List</span>
+              <span className="black">Purchase List</span>
             </NavLink>
           </li>
           <li className="nav-item pdl">
             <NavLink className="nav-link" to="/profile/myBooking">
-            <span className="black">My Booking</span>
+              <span className="black">My Booking</span>
             </NavLink>
           </li>
         </ul>
--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -10,7 +10,8 @@ class Profile extends Component {
   constructor() {
     super();
     this.state = {
-      info: undefined
+      info: undefined,
+      isLoaded: false
     };
   }
   async componentDidMount() {
@@ -24,38 +25,38 @@ class Profile extends Component {
       config
     );
     const { info } = response.data;
-    console.log(info);
-    this.setState({ info });
-    console.log(info);
-    console.log(config);
+    const isLoaded = true
+    this.setState({ info, isLoaded });
   }
   render() {
-    const { user } = this.props;
-    console.log(user);
-    const { info } = this.state;
-    console.log(info);
-    return (
-      <div className="container">
-        <div className="profile-container bglight mgtb">
-          <h1 className="profile-head">Profile</h1>
-          <div className="row">
-            <div className="col-md-4">
-              <ProfileBar userr={user} />
-            </div>
-            <div className="col-md-8 mt-5">
-              <ul>
-                <li>email: {user && user.info.email}</li>
-                <li>username: {user && user.info.username}</li>
-                <li>
-                  gender:
-                  {user && user.info.gender}
-                </li>
-              </ul>
+    const { isLoaded } = this.state
+    if (!isLoaded) {
+      return <h1>isLoading</h1>
+    } else {
+      const { user } = this.props
+      const { info } = this.state;
+      console.log(info)
+      return (
+        <div className="container">
+          <div className="profile-container bglight mgtb">
+            <h1 className="profile-head">Profile</h1>
+            <div className="row">
+              <div className="col-md-4">
+                <ProfileBar info={info} />
+              </div>
+              <div className="col-md-8 mt-5">
+                <ul>
+                  <li>email: {info.email}</li>
+                  <li>username: {info.username}</li>
+                  <li>name: {info.displayName}</li>
+                  <li>gender:{info.gender}</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
