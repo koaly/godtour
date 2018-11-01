2e134f4dae1271ab1640401b71e8f68128e9421e
nuttapon <nuttapon.wac@ku.th>
2018-11-01 23:50:48 +0700

    authService is god

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,8 +1,33 @@
 import http from "./httpService";
+import jwtDecode from "jwt-decode";
 import { apiUrl } from "../config.json";
 
 const apiEndpoint = apiUrl + "/users/login";
-
-export function login(email, password) {
-  return http.post(apiEndpoint, { email, password });
+const tokenKey = "token";
+export async function login(email, password) {
+  const { data: jwt } = await http.post(apiEndpoint, { email, password });
+  localStorage.setItem(tokenKey, jwt.user.token);
 }
+
+export function logout() {
+  localStorage.removeItem(tokenKey);
+}
+
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem(tokenKey);
+    return jwtDecode(jwt);
+  } catch (ex) {
+    return null;
+  }
+}
+export function getJwt() {
+  return localStorage.getItem("token");
+}
+
+export default {
+  login,
+  logout,
+  getCurrentUser,
+  getJwt
+};
