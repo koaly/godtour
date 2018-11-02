335c4c38c58ed4629bae0dbd3f92ddb9423671e6
Prempi <piyawat.pav@ku.th>
2018-11-02 11:01:45 +0700

    decorate profile page

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -4,6 +4,7 @@ import CancelBook from "./cancelBook";
 import MyBook from "./myBooking";
 import ProfileBar from "./common/profileBar";
 import Axios from "axios";
+import {MailIcon} from "mdi-react";
 //import { Route, Switch, Redirect } from "react-router-dom";
 
 export default class Profile extends Component {
@@ -37,13 +38,29 @@ export default class Profile extends Component {
             <div className="col-md-4">
               <ProfileBar user={user} />
             </div>
-            <div className="col-md-8 mt-5">
-              <ul>
+            <div className="col-md-8 mt-4">
+              {/*<ul>
                 <li>email: {user.info.email}</li>
                 <li>username: {user.info.username}</li>
                 <li>name: {user.info.displayName}</li>
                 <li>gender:{user.info.gender}</li>
-              </ul>
+              </ul>*/}
+              <div className="profile-infor mr-5">
+                <h4>Display Name</h4>
+                <h5>{user.info.displayName}</h5>
+              </div>
+              <div className="profile-infor mr-5">
+                <h4>Username</h4>
+                <h5>{user.info.username}</h5>
+              </div>
+              <div className="profile-infor mr-5">
+                <h4><MailIcon className="blue mr-2"/>Email Address</h4>
+                <h5>{user.info.email}</h5>
+              </div>
+              <div className="profile-infor mr-5">
+                <h4>Gender</h4>
+                <h5>{user.info.gender}</h5>
+              </div>
             </div>
           </div>
         </div>
