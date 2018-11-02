9bcccbf85d59ac9ea779ab58e381ccfa26237bf5
nuttapon <nuttapon.wac@ku.th>
2018-11-02 20:03:05 +0700

    refactor showtour2 move axios to tourservice and use Link god

--- client/src/components/showTour2.jsx
+++ client/src/components/showTour2.jsx
@@ -1,65 +1,55 @@
 import React, { Component } from "react";
 import Axios from "axios";
+import { getAllTours } from "../services/tourService";
+import { Link } from "react-router-dom";
 
 export default class showTour2 extends Component {
-    constructor() {
-        super()
-        this.state = {
-            tours: [],
-            isLoaded: false
-        }
+  constructor() {
+    super();
+    this.state = {
+      tours: [],
+      isLoaded: false
+    };
+  }
+  async getTour() {
+    this.setState({ isLoaded: false });
+    try {
+      const result = await getAllTours();
+      console.log(result.data);
+      const { tours } = result.data;
+      this.setState({ tours: tours });
+    } catch (e) {
+      console.log(e);
     }
-    async getTour(token) {
-        this.setState({ isLoaded: false })
-        const config = {
-            headers: { Authorization: "JWT " + token }
-        }
-        const url = 'http://localhost:5000/tours/browse'
-        try {
-            const result = await Axios.get(url, config)
-            console.log(result.data)
-            const { tours } = result.data
-            this.setState({ tours: tours })
-        }
-        catch (e) {
-            console.log(e)
-        }
-        this.setState({ isLoaded: true })
-
+    this.setState({ isLoaded: true });
+  }
+  componentDidMount() {
+    this.getTour();
+  }
+  render() {
+    const { tours, isLoaded } = this.state;
+    if (!isLoaded) {
+      return <h1>isLoaded</h1>;
     }
-    componentDidMount() {
-        const { token } = this.props
-
-        this.getTour(token)
+    console.log(tours);
+    if (!tours || tours.length == 0) {
+      return <h1>notFoundTour</h1>;
     }
-    getURL(id) {
-        return "http://localhost:3000/tours/id=" + id
-    }
-    render() {
-        const { tours, isLoaded } = this.state
-        if (!isLoaded) {
-            return <h1>isLoaded</h1>
-        }
-        console.log(tours)
-        if (!tours || tours.length == 0) {
-            return <h1>notFoundTour</h1>
-        }
-        return (
-            <div>
-                <h1>Tour List</h1>
-                <ul>
-                    {tours.map(tour =>
-                        <li key={tour._id}>
-                            <h3>
-                                {tour.name}
-                                <a href={this.getURL(tour._id)}>
-                                    next
-                                </a>
-                            </h3>
-                        </li>
-                    )}
-                </ul>
-            </div>
-        )
-    }
+    return (
+      <div>
+        <h1>Tour List</h1>
+        <ul>
+          {tours.map(tour => (
+            <li key={tour._id}>
+              <h3>
+                {tour.name}
+                {/* <a href={this.getURL(tour._id)}>next</a> */}
+                <Link to={`/tours/id=${tour._id}`}>next</Link>
+              </h3>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
--- client/src/services/tourService.js
+++ client/src/services/tourService.js
@@ -1,8 +1,12 @@
 import http from "./httpService";
 import { apiUrl } from "../config.json";
+import auth from "./authService";
 
 const apiEndpoint = apiUrl + "/tours/browse";
+const config = {
+  headers: { Authorization: "JWT " + auth.getJwt() }
+};
 
 export function getAllTours() {
-  return http.get(apiEndpoint);
+  return http.get(apiEndpoint, config);
 }
