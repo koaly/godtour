06d0dcb1896925f858d81f70a12684add375d3d3
nuttapon <nuttapon.wac@ku.th>
2018-11-03 17:52:58 +0700

    add addtour in tourservice

--- client/src/services/tourService.js
+++ client/src/services/tourService.js
@@ -2,6 +2,7 @@ import http from "./httpService";
 import auth from "./authService";
 
 const apiEndpoint = "/tours/browse";
+const tourAddEndpoint = "/tours/create";
 const config = {
   headers: {
     Authorization: "JWT " + auth.getJwt()
@@ -10,4 +11,49 @@ const config = {
 
 export function getAllTours() {
   return http.get(apiEndpoint, config);
+}
+
+export function addTour(tour) {
+  console.log(tour);
+  console.log(config);
+  const {
+    name,
+    price,
+    dest,
+    dayDuration,
+    nightDuration,
+    startBookDate,
+    startBookTime,
+    endBookDate,
+    endBookTime,
+    departDate,
+    returnDate,
+    airline,
+    seat,
+    food,
+    detail,
+    highlight
+  } = tour;
+  return http.post(
+    tourAddEndpoint,
+    {
+      name,
+      price,
+      dest,
+      dayDuration,
+      nightDuration,
+      startBookDate,
+      startBookTime,
+      endBookDate,
+      endBookTime,
+      departDate,
+      returnDate,
+      airline,
+      seat,
+      food,
+      detail,
+      highlight
+    },
+    config
+  );
 }
