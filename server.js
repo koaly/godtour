50d5de35687843cc9ec7ccfa27f6ebedfa8e2418
koaly <natthapong.s@ku.th>
2018-11-04 15:35:04 +0700

    back

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const port = process.env.PORT || 5000
 
 const server = http.createServer(app);
 
-// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
-// }
+}
 
 server.listen(port, () => console.log(`Server listen at port ${port}`));
 
