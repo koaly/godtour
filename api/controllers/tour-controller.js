b8e136b2731f2af1d9b33baef62fbce43cc94c4b
Key Person <pornmanuty@gmail.com>
2018-11-04 13:02:15 +0700

    everyone can now not remove

--- api/controllers/tour-controller.js
+++ api/controllers/tour-controller.js
@@ -81,14 +81,14 @@ exports.checkOwnTour = async (req, res, next) => {
         if (!tour || tour.length == 0) throw new TourNotFoundException()
 
         console.log(tour.operatorID);
-        if (info.status != 2 && info.id != tour.operatorID) {
+        if ((info.status == 2) || (info.id == tour.operatorID)) {
+            return next();
+        } else {
             return res.status(403).json({
                 error: {
                     message: "Permission denied"
                 }
             });
-        } else {
-            return next();
         }
 
     } catch (e) {
