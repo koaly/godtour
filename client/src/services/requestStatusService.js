bb4181c72fe7df84aaf547883e59c17e8ba79c34
nuttapon <nuttapon.wac@ku.th>
2018-11-05 00:13:22 +0700

    add requestStatus in requestStatus Service

--- client/src/services/requestStatusService.js
+++ client/src/services/requestStatusService.js
@@ -2,6 +2,7 @@ import http from "./httpService";
 import auth from "./authService";
 
 const apiEndpoint = "/api/admin/request/upgrade/";
+const requestStatusEndpoint = "/api/users/current/request/upgrade";
 const config = {
   headers: {
     Authorization: "JWT " + auth.getJwt()
@@ -11,4 +12,9 @@ const config = {
 export function acceptStatus(id) {
   return http.get(apiEndpoint + id + "/accept", config);
   //how to use put bro
+}
+
+export function requestStatus(data) {
+  const { reasonToUpgrade } = data;
+  return http.put(requestStatusEndpoint, { reasonToUpgrade }, config);
 }
