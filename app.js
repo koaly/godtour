e3a8db9cf48b2e4d766f061b28a2f35436ff9f70
koaly <natthapong.s@ku.th>
2018-11-04 14:40:29 +0700

    production

--- app.js
+++ app.js
@@ -107,5 +107,14 @@ app.use((err, req, res, next) => {
     })
 })
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 //export to server.js
 module.exports = app;
