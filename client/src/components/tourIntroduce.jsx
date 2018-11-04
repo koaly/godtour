cc0788a4c794ba5d805960fa39c6bdb068ed01fa
Prempi <piyawat.pav@ku.th>
2018-11-04 22:23:13 +0700

    tourIntro

--- client/src/components/tourIntroduce.jsx
+++ client/src/components/tourIntroduce.jsx
@@ -16,24 +16,25 @@ class TourIntroduce extends Component {
                         <h2 className="mt-5 mgbii">Why Tour With To-ur world ?</h2>
                         <div className="Introduce">
                             <h3>Can Create Your Own Tour</h3>
-                            <p>We will make your dream trip come true, just tell me your wish.</p>
+                            <p>We will make your dream trip come true, just tell me us wish.</p>
+                            <img src="https://cdn4.iconfinder.com/data/icons/world-travel-guide/512/travel-13-512.png" alt="" height="200px" width="200px"/>
                         </div>
                         <div className="Introduce">
-                            <h3>Can Create Your Own Tour</h3>
-                            <p>We will make your dream trip come true, just tell me your wish.</p>
+                            <h3>Save Money</h3>
+                            <p>Wonderful trip with low price and no booking fee.</p>
+                            <img src="https://cdn0.iconfinder.com/data/icons/finance-65/100/HAND_MONEY-512.png" alt="" height="200px" width="200px"/>
                         </div>
                         <div className="Introduce">
-                            <h3>Can Create Your Own Tour</h3>
-                            <p>We will make your dream trip come true, just tell me your wish.</p>
+                            <h3>Guarantee</h3>
+                            <p>If you have a problem during the trip, you will get your money back.</p>
+                            <img src="https://png.icons8.com/ios/1600/guarantee-filled.png" alt="" height="200px" width="200px"/>
                         </div>
                         <div className="Introduce">
-                            <h3>Can Create Your Own Tour</h3>
-                            <p>We will make your dream trip come true, just tell me your wish.</p>
+                            <h3>Special Discount</h3>
+                            <p>Special price for VIP user.</p>
+                            <img src="http://cdn.onlinewebfonts.com/svg/img_456356.png" alt="" height="200px" width="200px"/>
                         </div>
-                        <div className="Introduce">
-                            <h3>Can Create Your Own Tour</h3>
-                            <p>We will make your dream trip come true, just tell me your wish.</p>
-                        </div>
+                        
                         <Link to="/tours">
                             <button className="but">Book Tour Now</button>
                         </Link>
