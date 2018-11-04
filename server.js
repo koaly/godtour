10c1031c7e208ea602c47467d24cc038be7c7983
koaly <natthapong.s@ku.th>
2018-11-04 15:03:37 +0700

    add express to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const app = require('./app');
 const path = require('path');
+const express = require('express');
 const port = process.env.PORT || 5000
 
 const server = http.createServer(app);
