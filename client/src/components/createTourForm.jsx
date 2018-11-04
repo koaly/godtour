b758860685f0b3c8a11fc691289232e4cd0478ba
Supasan Komonlit <supasan_224@hotmail.com>
2018-11-05 01:24:58 +0700

    copy file from fetch

--- client/src/components/createTourForm.jsx
+++ client/src/components/createTourForm.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import listCountries from "./common/dataList";
+import findDiffDay from "./common/otherFunction"; 
+import { toast } from "react-toastify";
 
 class createTourForm extends Component{
 
@@ -8,42 +10,75 @@ class createTourForm extends Component{
 		super( props );
 		console.log("=====> constructor.props " , props);
 		this.state={
-			isLoading	: true
+			fillingForm	: true
 			, user		: props.user
 			, dataTour	: { country				: ""
 							, province			: ""
-							, dayDuration		: "0"
-							, nightDuration		: "0"
-							, numberChild		: "0"
-							, numberAdult		: "0"
+							, dayDuration		: 0
+							, nightDuration		: 0
+							, numberChild		: 0
+							, numberAdult		: 0
 							, privateTour		: true
 							, requireGuide		: false
-							, startPeriodTour	: ""
-							, endPeriodTour		: ""
+							, startPeriodTour	: null
+							, endPeriodTour		: null
 							, maxPrice			: "1000"
 							, specificDetail	: "Let us know what you want most in your tour"
 						  }
 		};
 		this.handleChange = this.handleChange.bind( this );
-		this.conditionVale = ["privateTour" , "requireGuide"]
+		this.handleSubmitData = this.handleSubmitData.bind( this );
+		this.notReloadFunction = this.notReloadFunction.bind( this );
+		this.conditionValue = ["privateTour" , "requireGuide"]
+		this.numericalValue = [	"dayDuration" , "nightDuration" , "numberChild" , "numberAdult"];
 	}
 
+	notReloadFunction(){
+		return false;
+	}
+
+	handleSubmitData( event ){
+		console.log( "===============>createTourForm.handleSubmitData " );
+		var result = document.getElementById("createTourForm").checkValidity();
+		console.log( "=====> handleSubmitData.result " , result );
+		if( this.state.dataTour.startPeriodTour === null 
+					|| this.state.dataTour.endPeriodTour === null){
+			toast.error("require period tour");
+			result = false;
+		}
+		else{ 
+			var startPeriod = new Date( this.state.dataTour.startPeriodTour );
+			var endPeriod = new Date( this.state.dataTour.endPeriodTour );
+			var freePeriod = findDiffDay(endPeriod , startPeriod);
+			if( this.state.dataTour.dayDuration > freePeriod ){
+				toast.error("Day of tour must less more than period of tour");
+			}
+		}
+		if( result ){
+			this.setState( state => ({
+				fillingForm : false
+			}))
+		}
+		return false;
+	}
 
 	handleChange( event ){
-		if( this.conditionVale.includes( event.target.name ) ){
+		if( this.conditionValue.includes( event.target.name ) ){
 			if( event.target.value === "true") this.state.dataTour[ event.target.name ] = true;
 			else this.state.dataTour[ event.target.name ] = false;
+		}
+		else if( this.numericalValue.includes( event.target.name ) ){
+			this.state.dataTour[ event.target.name ] = parseInt(event.target.value);
 		}
 		else{
 			this.state.dataTour[ event.target.name ] = event.target.value;
 		}
+		this.forceUpdate();
 		console.log( "=====> handleChange.state " , this.state);
-		this.setState();
 	}
 
 	componentDidMount() {
 		console.log("===============> createTourForm.componentDidMount");
-		sessionStorage.setItem("tourLastLink" , "/createTour");
 	}
 
 	render(){
@@ -53,25 +88,27 @@ class createTourForm extends Component{
 				<h1>Please Login Before Create Tour by Yourself</h1>
 			</div>);
 		}
-		else if( this.state.user.info.status === 0 ){
+		else if( this.state.user.info.status === 0 && this.state.fillingForm ){
 			return(<div className = "mgtb container">
 				<h1>Create Tour</h1>
-				<form action=""><ul>
+				<ul>
 					<li>
 						<label>Country : </label>
-						<input	type="textarea" list="listCountries" name="country"
-								onChange={this.handleChange} required 
+						<input	type="textarea" list="listCountries"
+								id = "createTourForm" 
+								name="country" required
+								onChange={this.handleChange}  
 						/>
 						<datalist id="listCountries">
 							{ listCountries.map( ( country) =>
-							<option key={country} value={ country }/>
+								<option key={country} value={ country }/>
 							)}
 						</datalist>
 					</li>
 					<li>
 						<label>Province : </label>
 						<input	type="textarea" name="province"
-								onChange={this.handleChange.bind}
+								onChange={this.handleChange}
 						/>
 					</li>
 					<li>
@@ -125,24 +162,24 @@ class createTourForm extends Component{
 					</li>
 					<li>
 						<label>Length Tour :&emsp;</label>
-						<label>Day</label>
+						<label>Day&emsp;</label>
 						<input	type="number" name="dayDuration" min="0"  
 								value={this.state.dataTour.dayDuration}
 								onChange={this.handleChange}
 						/>
-						<label>Night</label>
+						<label>Night&emsp;</label>
 						<input	type="number" name="nightDuration" min="0" 
 								value={this.state.dataTour.nightDuration}
 								onChange={this.handleChange}
 						/>
 					</li>
 					<li>
-						<label>Priod Tour :&emsp;</label>
-						<label>Start</label>
+						<label>Period Tour :&emsp;</label>
+						<label>Start&emsp;</label>
 						<input	type="date" name="startPeriodTour"
 								onChange={this.handleChange}
 						/>
-						<label>End</label>
+						<label>End&emsp;</label>
 						<input	type="date" name="endPeriodTour"
 								onChange={this.handleChange}
 						/>
@@ -158,12 +195,25 @@ class createTourForm extends Component{
 					<li>
 						<p>Message to Tourism</p>
 						<textarea	name="specificDetail" cols="60" rows="5" 
-									onChange={this.handleChange}>
-							{this.state.dataTour.specificDetail}	
-						</textarea>
+									onChange={this.handleChange}
+									placeholder="Let us know what special do you want"
+						></textarea>
 					</li>
-					<input type="submit"/>	
-				</ul></form>
+					<button onClick={this.handleSubmitData}>SUBMIT</button>	
+				</ul>
+			</div>);
+		}
+		else if( this.state.user.info.status === 0 ){
+			const tour = this.state.dataTour; 
+			return(<div>
+				<h1>Summany request create tour by {this.state.user.info.displayName}</h1>
+				<ul>
+					<li>Country  : {tour.country}</li>
+					<li>Province : {tour.province==="" ? "-" : tour.province }</li>
+					<li>
+						Duration Tour : {tour.dayDuration} Day(s) : {tour.nightDuration} night(s)
+					</li>
+				</ul>
 			</div>);
 		}
 		else{
