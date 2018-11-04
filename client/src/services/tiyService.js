f0cfde0e35500a0f0fe111f3db54ca94f256feca
Key Person <pornmanuty@gmail.com>
2018-11-04 22:54:58 +0700

    fix confict

--- client/src/services/tiyService.js
+++ client/src/services/tiyService.js
@@ -2,10 +2,6 @@ import http from "./httpService";
 import auth from "./authService";
 
 const apiEndpoint = "/api/users/current/offers";
-<<<<<<< HEAD
-
-=======
->>>>>>> b1b4d7fe9e369b9b297a989063bd8309ed7ab9cd
 
 const config = {
     headers: {
