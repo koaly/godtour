1dbf522db4e15a88fa7f10b829fdbbd9ae8681c9
nuttapon <nuttapon.wac@ku.th>
2018-11-05 04:55:13 +0700

    getOwnTiy

--- client/src/services/tiyService.js
+++ client/src/services/tiyService.js
@@ -2,13 +2,18 @@ import http from "./httpService";
 import auth from "./authService";
 
 const apiEndpoint = "/api/users/current/offers";
+const tiyEndpoint = "/api/users/current/tiys";
 
 const config = {
-    headers: {
-        Authorization: "JWT " + auth.getJwt()
-    }
+  headers: {
+    Authorization: "JWT " + auth.getJwt()
+  }
 };
 
 export function getOwnOffer() {
-    return http.get(apiEndpoint, config);
+  return http.get(apiEndpoint, config);
+}
+
+export function getOwnTiy() {
+  return http.get(tiyEndpoint, config);
 }
