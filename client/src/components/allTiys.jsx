ce3cc1d96215b779b70707bfdc766f7b51509f0b
Prempi <piyawat.pav@ku.th>
2018-12-01 19:05:37 +0700

    change profile-head color due to ceo's request

--- client/src/components/allTiys.jsx
+++ client/src/components/allTiys.jsx
@@ -9,14 +9,15 @@ class AllTiys extends Component {
     super(props);
     this.state = {
       isLoaded: false,
-      tiys: []
+      tiys: [],
+      data: []
     };
   }
   async componentDidMount() {
     try {
       const { data } = await getAllTiys();
       const { tiys } = data;
-      this.setState({ tiys, isLoaded: true });
+      this.setState({ tiys, isLoaded: true, data});
       console.log(data);
       console.log(tiys);
     } catch (ex) {
@@ -26,7 +27,7 @@ class AllTiys extends Component {
   }
 
   render() {
-    const { tiys, isLoaded } = this.state;
+    const { tiys, isLoaded, data} = this.state;
     console.log(tiys);
     if (!isLoaded) {
       return (
@@ -39,23 +40,35 @@ class AllTiys extends Component {
     }
 
     return (
-      <div className="container">
-        <h1>hello</h1>
+      <div className="container mgtb">
+        <div className="user-content mx-3 my-1">
+          <h1 className="user-head">{data.count} Tiys in database</h1>
+        </div>
         {isLoaded &&
           tiys.map((t, index) => (
-            <div key={index} className="row">
-              <li>{t.name}</li>
-              <Link
-                className="btn btn-primary ml-4 mb-5"
-                to={{
-                  pathname: `/${t._id}/createOffer`,
-                  state: {
-                    tiys: t
-                  }
-                }}
-              >
-                Create offer
-              </Link>
+            <div key={index} className="user-content mx-3 my-3">
+                <div className="profile-infor mx-1 my-1">
+                  <div className="row">
+                    <ul>
+                      <div className="ml-3">
+                        {t.name}
+                      </div>
+                    </ul>
+                    <Link
+                      // className="btn btn-primary ml-4 mb-5"
+                      to={{
+                        pathname: `/${t._id}/createOffer`,
+                        state: {
+                          tiys: t
+                        }
+                      }}
+                    >
+                      <div>
+                        <button className="btn btn-primary ml-4 mb-5 float-right">Create offer</button>
+                      </div>
+                    </Link>
+                  </div>
+                </div>
             </div>
           ))}
       </div>
