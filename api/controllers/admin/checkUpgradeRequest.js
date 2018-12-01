173b1655211db5bd32f4ee0dfb7b4f4aaecb7973
Key Person <pornmanuty@gmail.com>
2018-12-01 17:47:03 +0700

    cancel validation

--- api/controllers/admin/checkUpgradeRequest.js
+++ api/controllers/admin/checkUpgradeRequest.js
@@ -1,18 +1,16 @@
 const {
   UserNotFoundException,
-  ObjectIdIsNotValidException,
-  NoRequestUpgrade
+  NoRequestUpgrade,
+  Request
 } = require("../utility/exception");
-const { checkObjectIdIsValid } = require("../utility/");
-
+const User = require("../../models/user-models");
 const handle = async (req, res, next) => {
   const {
-    params: { id }
+    params: { username }
   } = req;
 
-  if (checkObjectIdIsValid(id)) next(new ObjectIdIsNotValidException());
-
-  const user = await user.findOne({ _id: id });
+  if (!username) next(new Request("username"));
+  const user = await User.findOne({ username: username });
 
   if (!user) next(new UserNotFoundException());
   if (!user.upgradeRequest) next(new NoRequestUpgrade());
--- api/controllers/operator/index.js
+++ api/controllers/operator/index.js
@@ -1,1 +1,3 @@
 exports.checkOperatorStatus = require("./checkOperatorStatus");
+exports.checkNonOperatorStatus = require("./checkNonOperatorStatus");
+exports.checkAdminStatus = require("./checkAdminStatus");
