9bed4a84cb66ae20aaa7884c26f1feb54d2ba093
Supasan Komonlit <supasan_224@hotmail.com>
2018-12-02 16:57:45 +0700

    Update Screen

--- client/src/components/tour/allTiys.jsx
+++ client/src/components/tour/allTiys.jsx
@@ -9,12 +9,18 @@ class AllTiys extends Component {
     super(props);
     this.state = {
       isLoaded: false,
-      tiys: []
+      tiys: [],
+	  mode: 0
     };
+	this.firstStyle = " width30";
+	this.secondStyle = " width20";
+	this.thirdStyle = " width15";
   }
 
   async componentDidMount() {
     this.downloadTiys();
+	this.updateDimension();
+	window.addEventListener("resize" , this.updateDimension.bind( this ) );
   }
 
   async downloadTiys() {
@@ -35,26 +41,63 @@ class AllTiys extends Component {
 		console.log(tiys);
 	}
 
+	updateDimension(){
+		let width = window.innerWidth;
+		console.log("width is " , width );
+		if( width > 660 ){
+			this.firstStyle = " width30";
+			this.secondStyle = " width20";
+			this.thirdStyle = " width15";
+			this.setState( { mode : 0 } );	
+		}
+		else if( width > 440 ){
+			this.firstStyle = " width35";
+			this.secondStyle = " width25";
+			this.setState( { mode : 1});
+		}
+		else{
+			this.firstStyle = " width40";
+			this.secondStyle = " width35";
+			this.setState( { mode : 2});
+		}
+	}
+
+	componentWillUnmound(){
+		window.removeEventListener("resize" , this.updateDimension.bind( this ) );
+	}
+
 	render() {
-		const { tiys, isLoaded } = this.state;
+		const { tiys, isLoaded , mode} = this.state;
 		console.log(tiys);
 		return (
 		<div className=" minWidth100  ">
 			<h1 className="minWidth100 textCenter">List Tour by Customer</h1>
 			<div className=" minWidth100 clearBoth  headColumn">
-				<div className="width30 textLeft paddingLeft4 sameLine ">NAME TOUR</div>
-				<div className="width20 textCenter sameLine">DESTINATIOn</div>
-				<div className="width20 textCenter sameLine">PRICE</div>
-				<div className="width15 textCenter sameLine">DURATION</div>
+				<div className={"textLeft paddingLeft4 sameLine" + this.firstStyle}>
+					NAME TOUR</div>
+				<div className={"textCenter sameLine" + this.secondStyle}>
+					DESTINATIOn</div>
+				{ mode < 2 &&
+				<div className={"textCenter sameLine" + this.secondStyle}>
+					PRICE</div>}
+				{ mode < 1 &&
+				<div className={"textCenter sameLine" + this.thirdStyle}>
+					DURATION</div>}
 			</div>
 			{isLoaded && tiys.map(t => (
 				<div className="">
-					<p key={t._id} className="sameLine width30 paddingLeft2">{t.name}</p>
-					<p className="sameLine width20 textCenter">{t.dest}</p>
-					<p className="sameLine width20 textCenter">{t.minPrice} - {t.maxPrice}</p>
-					<p className="sameLine width15 textCenter">{t.minDuration} - {t.maxDuration}</p>
+					<p key={t._id} className={"sameLine paddingLeft2" + this.firstStyle}>
+						{t.name}</p>
+					<p className={"sameLine textCenter" + this.secondStyle}>
+						{t.dest}</p>
+					{ mode < 2 &&
+					<p className={"sameLine textCenter" + this.secondStyle}>
+						{t.minPrice} - {t.maxPrice}</p>}
+					{ mode < 1 &&
+					<p className={"sameLine textCenter" + this.thirdStyle}>
+						{t.minDuration} - {t.maxDuration}</p>}
 					<Link
-		                className="btn btn-primary sameLine"
+		                className="btn btn-primary sameLine width10"
 		                to={{
 		                  pathname: `/${t._id}/createOffer`,
 		                  state: {
