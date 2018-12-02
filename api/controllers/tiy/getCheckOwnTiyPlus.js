ce466a1ba7b3af3d4e96721a83bf105b92bcb1f3
Key Person <pornmanuty@gmail.com>
2018-12-02 14:53:45 +0700

    i out

---
+++ api/controllers/tiy/getCheckOwnTiyPlus.js
@@ -0,0 +1,34 @@
+const Tiy = require("../../models/tiy-models");
+const { asynWrapper, checkObjectIdIsValid } = require("../utility");
+const {
+  Request,
+  ObjectIdIsNotValidException,
+  NoPermissonAccess,
+  TiyNotFound
+} = require("../utility/exception");
+
+const handle = async (req, res, next) => {
+  const {
+    payload: {
+      info: { id: userID, status: userStatus }
+    },
+    params: { tiyID }
+  } = req;
+
+  //request tiyid
+  if (!tiyID) throw new Request("TiyID");
+  //check tiyID is vaild
+  if (!checkObjectIdIsValid(tiyID)) throw new ObjectIdIsNotValidException();
+
+  //find id
+  const tiy = await Tiy.findOne({ _id: tiyID });
+  if (!tiy) throw new TiyNotFound();
+
+  //check if not admin or not own return
+  if (userStatus == 2 || userStatus == 1 || userID == tiy.userID) {
+    res.locals.tiy = tiy;
+    next();
+  } else throw new NoPermissonAccess();
+};
+
+module.exports = asynWrapper.bind(null, handle);
